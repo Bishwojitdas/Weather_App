d95f9f03d15bf6fadfafd97490bbc6c6542952e0
Bishwojitdas <bishwoji88@gmail.com>
2023-10-27 14:48:33 +0600

    hello

--- lib/screen/todays_weather/todays_weather.dart
+++ lib/screen/todays_weather/todays_weather.dart
@@ -7,6 +7,10 @@ class TodaysWeather extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Column(
+
+      ),
+    );
   }
 }
